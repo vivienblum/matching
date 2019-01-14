be56c3989fb20b1a75e5b896dc3a1c826e1cff73
vivien-mac <vivien@iMac-de-Nicolas-mac.local>
2019-01-14 11:38:04 +0100

    :card_file_box: Models ands migrations

--- images/models.py
+++ images/models.py
@@ -3,4 +3,18 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Collection(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Item(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='item_image')
+    collection = models.ForeignKey(Collection, related_name='items', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
