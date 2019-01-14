2ffb3840dbb881982204e9c1ceafec4c71f85acb
vivien-mac <vivien@iMac-de-Nicolas-mac.local>
2019-01-14 11:43:56 +0100

    :bento: Added views

--- images/views.py
+++ images/views.py
@@ -3,4 +3,14 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+from .serializers import CollectionSerializer, ItemSerializer
+from .models import Collection, Item
+
+class CollectionViewSet(ModelViewSet):
+    serializer_class = CollectionSerializer
+    queryset = Collection.objects.all()
+
+class ItemViewSet(ModelViewSet):
+    serializer_class = ItemSerializer
+    queryset = Item.objects.all()
