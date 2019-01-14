51cbd3e1b3deba975014ce7bf84d11027304392e
vivien-mac <vivien@iMac-de-Nicolas-mac.local>
2019-01-14 13:32:25 +0100

    :card_file_box: Added resources to admin

--- images/admin.py
+++ images/admin.py
@@ -3,4 +3,7 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from .models import Collection, Item
+
+admin.site.register(Collection)
+admin.site.register(Item)
