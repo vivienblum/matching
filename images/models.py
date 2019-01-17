78a0103b2658100914fd413b216cc30f5c7e94b8
vivien-mac <vivien@iMac-de-Nicolas-mac.local>
2019-01-17 18:03:50 +0100

    :zap: Find the best item

--- images/models.py
+++ images/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from itertools import chain
 from django.db import models
 
 DELTA = 25
@@ -17,7 +17,84 @@ class ItemManager(models.Manager):
         if delta == None:
             delta = 25
         delta = int(delta)
-        return self.filter(collection=collection).filter(blue__lte=color[0] + delta ).filter(blue__gte=color[0] - delta ).filter(green__lte=color[1] + delta ).filter(green__gte=color[1] - delta ).filter(red__lte=color[2] + delta ).filter(red__gte=color[2] - delta).first()
+        blue_less = (
+            self.filter(collection=collection)
+                    .filter(blue__lte=color[0])
+                    .filter(blue__gte=color[0] - delta)
+                    .order_by('-blue')
+        )
+        blue_greather = (
+            self.filter(collection=collection)
+                    .filter(blue__gte=color[0])
+                    .filter(blue__lte=color[0] + delta)
+                    .order_by('blue')
+        )
+        matches_blue = list(chain(blue_less, blue_greather))
+
+        green_less = (
+            self.filter(collection=collection)
+                    .filter(green__lte=color[1])
+                    .filter(green__gte=color[1] - delta)
+                    .order_by('-green')
+        )
+        green_greather = (
+            self.filter(collection=collection)
+                    .filter(green__gte=color[1])
+                    .filter(green__lte=color[1] + delta)
+                    .order_by('green')
+        )
+        matches_green = list(chain(green_less, green_greather))
+
+        red_less = (
+            self.filter(collection=collection)
+                    .filter(red__lte=color[2])
+                    .filter(red__gte=color[2] - delta)
+                    .order_by('-red')
+        )
+        red_greather = (
+            self.filter(collection=collection)
+                    .filter(red__gte=color[2])
+                    .filter(red__lte=color[2] + delta)
+                    .order_by('red')
+        )
+        matches_red = list(chain(red_less, red_greather))
+        items_match = list(set(matches_blue) & set(matches_green) & set(matches_red))
+        # Find the closest
+        best_item = None
+        min = 255
+        for item in items_match:
+            default_blue = abs(item.blue - color[0])
+            default_green = abs(item.green - color[1])
+            default_red = abs(item.red - color[2])
+            default = default_blue + default_green + default_red
+            if default <= min:
+                min = default
+                best_item = item
+                
+        return best_item
+
+
+        # # print (list(set().intersection(matches_blue, matches_green, matches_red)))
+        #
+        # return (
+        #     # self.filter(collection=collection)
+        #     #         .filter(blue__lte=color[0] + delta)
+        #     #         .filter(blue__gte=color[0] - delta)
+        #     #         .filter(green__lte=color[1] + delta)
+        #     #         .filter(green__gte=color[1] - delta)
+        #     #         .filter(red__lte=color[2] + delta)
+        #     #         .filter(red__gte=color[2] - delta)
+        #     #         .first()
+        #     #     )
+        #     self.filter(collection=collection)
+        #             .filter(blue__lte=color[0])
+        #             .filter(blue__gte=color[0] - delta)
+        #             .filter(green__lte=color[1] + delta)
+        #             .filter(green__gte=color[1] - delta)
+        #             .filter(red__lte=color[2] + delta)
+        #             .filter(red__gte=color[2] - delta)
+        #             .first()
+        #         )
 
 class Item(models.Model):
     name = models.CharField(max_length=200)
