423f2f5133ad91d702626465fd4a474582319f0b
vivien-mac <vivien@iMac-de-Nicolas-mac.local>
2019-01-21 14:38:54 +0100

    :see_no_evil: Updated gitingnore

--- images/models.py
+++ images/models.py
@@ -2,6 +2,8 @@
 from __future__ import unicode_literals
 from itertools import chain
 from django.db import models
+from django.dispatch import receiver
+from django.utils.translation import ugettext_lazy as _
 
 DELTA = 25
 
@@ -13,66 +15,6 @@ class Collection(models.Model):
         return self.name
 
 class ItemManager(models.Manager):
-    # def get_item_color(self, color, collection, delta):
-    #     if delta == None:
-    #         delta = 25
-    #     delta = int(delta)
-    #     blue_less = (
-    #         self.filter(collection=collection)
-    #                 .filter(blue__lte=color[0])
-    #                 .filter(blue__gte=color[0] - delta)
-    #                 .order_by('-blue')
-    #     )
-    #     blue_greather = (
-    #         self.filter(collection=collection)
-    #                 .filter(blue__gte=color[0])
-    #                 .filter(blue__lte=color[0] + delta)
-    #                 .order_by('blue')
-    #     )
-    #     matches_blue = list(chain(blue_less, blue_greather))
-    #
-    #     green_less = (
-    #         self.filter(collection=collection)
-    #                 .filter(green__lte=color[1])
-    #                 .filter(green__gte=color[1] - delta)
-    #                 .order_by('-green')
-    #     )
-    #     green_greather = (
-    #         self.filter(collection=collection)
-    #                 .filter(green__gte=color[1])
-    #                 .filter(green__lte=color[1] + delta)
-    #                 .order_by('green')
-    #     )
-    #     matches_green = list(chain(green_less, green_greather))
-    #
-    #     red_less = (
-    #         self.filter(collection=collection)
-    #                 .filter(red__lte=color[2])
-    #                 .filter(red__gte=color[2] - delta)
-    #                 .order_by('-red')
-    #     )
-    #     red_greather = (
-    #         self.filter(collection=collection)
-    #                 .filter(red__gte=color[2])
-    #                 .filter(red__lte=color[2] + delta)
-    #                 .order_by('red')
-    #     )
-    #     matches_red = list(chain(red_less, red_greather))
-    #     items_match = list(set(matches_blue) & set(matches_green) & set(matches_red))
-    #     # Find the closest
-    #     best_item = None
-    #     min = 255
-    #     for item in items_match:
-    #         default_blue = abs(item.blue - color[0])
-    #         default_green = abs(item.green - color[1])
-    #         default_red = abs(item.red - color[2])
-    #         default = default_blue + default_green + default_red
-    #         if default <= min:
-    #             min = default
-    #             best_item = item
-    #
-    #     return best_item
-
     def get_item_color(self, color, collection, delta):
         if delta == None:
             delta = 25
@@ -87,7 +29,6 @@ class ItemManager(models.Manager):
             .filter(red__lte=color[2] + delta )
             .filter(red__gte=color[2] - delta)
         )
-        # print(items_match)
         # Find the closest
         best_item = None
         min = 255
