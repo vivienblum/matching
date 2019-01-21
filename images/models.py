43c25b6f9ddb17b4ca9f7464ff44f2e1ffdb9343
vivien-mac <vivien@iMac-de-Nicolas-mac.local>
2019-01-21 14:46:50 +0100

    :sparkles: Delete image file on delete model

--- images/models.py
+++ images/models.py
@@ -4,6 +4,7 @@ from itertools import chain
 from django.db import models
 from django.dispatch import receiver
 from django.utils.translation import ugettext_lazy as _
+import os
 
 DELTA = 25
 
@@ -55,6 +56,27 @@ class Item(models.Model):
 
     def __str__(self):
         return self.name
+
+@receiver(models.signals.post_delete, sender=Item)
+def auto_delete_file_on_delete(sender, instance, **kwargs):
+    if instance.image:
+        if os.path.isfile(instance.image.path):
+            os.remove(instance.image.path)
+
+@receiver(models.signals.pre_save, sender=Item)
+def auto_delete_file_on_change(sender, instance, **kwargs):
+    if not instance.pk:
+        return False
+
+    try:
+        old_file = Item.objects.get(pk=instance.pk).image
+    except Item.DoesNotExist:
+        return False
+
+    new_file = instance.image
+    if not old_file == new_file:
+        if os.path.isfile(old_file.path):
+            os.remove(old_file.path)
 
 class Match(object):
     def __init__(self, **kwargs):
