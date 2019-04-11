989431d3bf36fbe793fecb53ea4fd01ac1781864
Vivien BLUM <vivien@playplay.video>
2019-04-11 16:36:49 +0200

    Set db dynamic

--- matching/settings.py
+++ matching/settings.py
@@ -86,6 +86,10 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 ALLOWED_HOSTS = ['match-images.herokuapp.com', 'localhost', '127.0.0.1']
 
 CORS_ORIGIN_ALLOW_ALL = True
