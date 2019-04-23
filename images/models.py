606190dce943544dc651de1b1611b98983c07322
Vivien BLUM <vivien@playplay.video>
2019-04-23 16:30:11 +0200

    :lock: Fixed issue on deleting item

--- images/models.py
+++ images/models.py
@@ -81,24 +81,3 @@ class Match(models.Model):
 
     def __str__(self):
         return self.id
-
-@receiver(models.signals.post_delete, sender=Item)
-def auto_delete_file_on_delete(sender, instance, **kwargs):
-    if instance.image:
-        if os.path.isfile(instance.image.path):
-            os.remove(instance.image.path)
-
-@receiver(models.signals.pre_save, sender=Item)
-def auto_delete_file_on_change(sender, instance, **kwargs):
-    if not instance.pk:
-        return False
-
-    try:
-        old_file = Item.objects.get(pk=instance.pk).image
-    except Item.DoesNotExist:
-        return False
-
-    new_file = instance.image
-    if not old_file == new_file:
-        if os.path.isfile(old_file.path):
-            os.remove(old_file.path)
