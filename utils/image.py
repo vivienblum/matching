8f7c03ea3484a82156c59d90e4d3cd9d9843f170
Vivien BLUM <vivien@playplay.video>
2019-05-13 09:31:12 +0200

    :lock: Fixed issue on delta Collection

--- utils/image.py
+++ utils/image.py
@@ -72,7 +72,7 @@ def match_images(id):
     match = Match.objects.get(pk=id)
     image = pixelate(match)
     collection = match.collection
-    delta = Collection.objects.get(pk=collection).delta
+    delta = match.collection.delta
 
     h = image.shape[0]
     w = image.shape[1]
