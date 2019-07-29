65fc0cf2d52ced4838da323416fffc10070bd084
Vivien BLUM <vivien@playplay.video>
2019-07-29 15:19:06 +0200

    :sparkles: Added quantity on items

--- matching/settings.py
+++ matching/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
 CACHES = {
     "default": {
          "BACKEND": "redis_cache.RedisCache",
-         "LOCATION": os.environ.get('REDIS_URL'),
+         "LOCATION": os.environ.get('REDIS_URL', 'redis://localhost:6379'),
     }
 }
 
